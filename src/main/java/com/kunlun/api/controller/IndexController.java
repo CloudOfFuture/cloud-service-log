a8081829d070713c041a4b9ccfedfd219bc8e25d
Jarven <769558579@qq.com>
2017-12-21 14:34:45 +0800

    测试服务之间互相调用

--- src/main/java/com/kunlun/api/controller/IndexController.java
+++ src/main/java/com/kunlun/api/controller/IndexController.java
@@ -4,6 +4,7 @@ import com.kunlun.result.DataRet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @created on 2017/12/21.
  */
 @RestController
+@RequestMapping("index")
 public class IndexController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
 
-    @GetMapping("/test")
+    @GetMapping("/log")
     public DataRet<String> test(@RequestParam("order_id") Long orderId) {
         LOGGER.info("订单服务传递过来的值: {}", orderId);
         return new DataRet<>("成功接收");
