39188e0737cfdd17cbb27696df17f0b1d4774dec
MoonHong <1049313183@qq.com>
2017-12-22 16:09:54 +0800

    商品日志

---
+++ src/main/java/com/kunlun/api/controller/GoodLogController.java
@@ -0,0 +1,27 @@
+package com.kunlun.api.controller;
+
+import com.kunlun.result.DataRet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author by hmy
+ * @version <0.1>
+ * @created on 2017-12-22.
+ */
+@RestController
+@RequestMapping("goodLog")
+public class GoodLogController {
+
+
+
+    @GetMapping("/saveGoodLog")
+    public DataRet<String> saveGoodLog(@RequestParam("good_name") String goodName,
+                                       @RequestParam("action") String action,
+                                       @RequestParam("id") Long id){
+        return null;
+    }
+}
---
+++ src/main/java/com/kunlun/api/service/GoodLogService.java
@@ -0,0 +1,9 @@
+package com.kunlun.api.service;
+
+/**
+ * @author by hmy
+ * @version <0.1>
+ * @created on 2017-12-22.
+ */
+public interface GoodLogService {
+}
